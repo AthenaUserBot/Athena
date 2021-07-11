a560b29d68393f6a385ebc10a6931c5f61a903c2
Mehmet <86586138+wiperowner@users.noreply.github.com>
2021-07-11 13:25:43 +0300

    Add files via upload

---
+++ athena/plugins/misc/all_athena.py
@@ -0,0 +1,32 @@
+#AthenaUserBot | All plugini
+from athena import bot as app
+from core.muin import muinrobot
+from asyncio import sleep
+
+@muinrobot(outgoing=True, pattern="^.all(?: |$)(.*)")
+async def alll(q):
+
+    chat = q.chat.id
+    a_=0
+    await q.delete()
+    async for member in app.iter_chat_members(chat):
+        if a_ == 5000:
+            break
+        a_+=1
+        await app.send_message(chat, "[{}](tg://user?id={}) {}".format(member.user.first_name, member.user.id))
+        await sleep(4)
+
+
+@muinrobot(outgoing=True, pattern="^.alladmin(?: |$)(.*)")
+async def _(q):
+    chat = q.chat.id
+    a_=0
+    await q.delete()
+    admin_list = [i.user for i in await bot.get_chat_members(call.message.chat.id, filter="administrators")]
+    for i in admin_list:
+        if a_ == 5000:
+            break
+        a_+=1
+        await app.send_message(chat, "[{}](tg://user?id={}) {}".format(i.first_name, i.id))
+        await sleep(4)
+
