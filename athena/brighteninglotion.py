34fc1dc7f98c1d29d6975a30c67b448bc65fff04
berce <75228263+must4f@users.noreply.github.com>
2021-07-11 20:07:24 +0300

    Update brighteninglotion.py

--- athena/brighteninglotion.py
+++ athena/brighteninglotion.py
@@ -1,5 +1,11 @@
 from . import *
+from pyrogram import idle
 
+try:
+    bot.start()
+    print('Athena çalışıyor. Sorununuz varsa t.me/athenasupport a gelin.')
+    idle()
+except Exception as e:
+    print('Bir hata oluştu: {}'.format(str(e)))
 
-bot.run()
 
