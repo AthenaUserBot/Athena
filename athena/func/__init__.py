1fbb1b109577fbde9e8f1d3f7db893493cfad3fc
berce <75228263+must4f@users.noreply.github.com>
2021-07-11 14:30:38 +0300

    Update __init__.py

--- athena/func/__init__.py
+++ athena/func/__init__.py
@@ -1,5 +1,5 @@
 async def extract_text(message):
     text = message.text.markdown or message.caption.markdown
     if not text:
-        return None
+        return ''
     return text.split()[1] if len(text.split()) > 1 else None
