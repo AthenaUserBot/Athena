42bc208fd149d1f2b1e0c7b83a69d29d7572eaf9
berce <75228263+must4f@users.noreply.github.com>
2021-07-11 14:48:29 +0300

    Update all.py

--- athena/plugins/misc/all.py
+++ athena/plugins/misc/all.py
@@ -1,7 +1,7 @@
 #AthenaUserBot | All plugini
 
+from athena import bot as app, TEMP, BOT_NAME
 from athena.func import extract_text
-from athena import bot as app
 from core.muin import muinrobot
 from asyncio import sleep
 
@@ -9,18 +9,24 @@ from asyncio import sleep
 async def alll(q):
     sebep = await extract_text(q)
 
+    ben = TEMP['ME'].id
+
     chat = q.chat.id
 
     a_=0
-    seb = f'"{sebep}' + 'için"' if sebep else ''
+    seb = f'"{sebep} ' + 'için"' if sebep not '' else ''
     await q.edit_text(f'**🔄 Athena {seb} etiketlemeyi başlatıyor..**')
 
     async for member in app.iter_chat_members(chat):
         if a_ == 5000:
             break
-        a_+=1
+        if member.user.id == ben:
+            continue
+        else:
+            a_+=1
         await app.send_message(chat, "[{}](tg://user?id={}) {}".format(member.user.first_name, member.user.id, sebep))
         await sleep(3)
+    await app.send_message(chat,f"**✅ {BOT_NAME} etiketleme işlemini bitirdi..\n🗒️ Toplam {a_} kişi etiketlendi..**")
 
 
 @muinrobot(outgoing=True, pattern="^.alladmin(?: |$)(.*)")
@@ -28,12 +34,20 @@ async def alladmin(q):
     sebep = await extract_text(q)
     chat = q.chat.id
     a_=0
-    await q.delete()
+
+    ben = TEMP['ME'].id
+
+    seb = f'"{sebep} ' + 'için"' if sebep not '' else ''
+    await q.edit_text(f'**🔄 {BOT_NAME} {seb} admin etiketlemesini başlatıyor..**')
     admin_list = [i.user for i in await bot.get_chat_members(chat, filter="administrators")]
     for i in admin_list:
         if a_ == 5000:
             break
-        a_+=1
+        if member.user.id == ben:
+            continue
+        else:
+            a_+=1
         await app.send_message(chat, "[{}](tg://user?id={}) {}".format(i.first_name, i.id,sebep))
         await sleep(3)
+    await app.send_message(chat,f"**✅ {BOT_NAME} admin etiketleme işlemini bitirdi..**")
 
