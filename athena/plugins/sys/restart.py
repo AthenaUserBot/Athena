8a271bcc1511a111042fc9b68b9e4d248a0279a7
berce <75228263+must4f@users.noreply.github.com>
2021-07-13 23:21:35 +0300

    Update restart.py

--- athena/plugins/sys/restart.py
+++ athena/plugins/sys/restart.py
@@ -1,13 +1,13 @@
 from athena import BOT_NAME, bot as app
 from athena.func import to_be_sent
 from core.muin import muinrobot
-
+import os,sys
 
 @muinrobot(pattern='^.restart')
 async def restart(event):
     await event.edit_text(f'🔄 {BOT_NAME} yeniden başlatılıyor..')
     try:
-        await app.restart()
+        await app.stop()
     except:
         pass
-    await event.edit_text(f'✅ {BOT_NAME} yeniden başlatıldı..')
+    os.execl(sys.executable, sys.executable, *sys.argv)
