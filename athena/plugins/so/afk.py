65e3d887b815effc96997c9937ec5df2847238e9
berce <75228263+must4f@users.noreply.github.com>
2021-07-14 00:00:48 +0300

    Update afk.py

--- athena/plugins/so/afk.py
+++ athena/plugins/so/afk.py
@@ -6,5 +6,7 @@ from athena.func import it, ct
 async def afkmodon(message):
     global AFKMOD
     if AFMOD:
-        return await message.edit_text(await it("😳 Şuanda zaten a-afk'sın!"))
-    await message.edit_text(await ct("🥺 Artık ekrandan çok uzaktayım.."))
+        text = await it("😳 Şuanda zaten a-afk'sın!")
+        return await message.edit_text(text)
+    text = await ct("🥺 Artık ekrandan çok uzaktayım..")
+    await message.edit_text(text)
