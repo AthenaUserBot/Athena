bbe988e0ad4a7c6d5d335d2143713f50d3bd6591
berce <75228263+must4f@users.noreply.github.com>
2021-07-14 01:10:08 +0300

    Update afk.py

--- athena/plugins/so/afk.py
+++ athena/plugins/so/afk.py
@@ -1,12 +1,21 @@
-from athena import AFKMOD
+from athena import AFKMOD, bot
 from core.muin import muinrobot
 from athena.func import it, ct
 
 @muinrobot(pattern='^.afk')
 async def afkmodon(message):
     global AFKMOD
-    if AFMOD:
+    if AFKMOD:
         text = await it("😳 Şuanda zaten a-afk'sın!")
         return await message.edit_text(text)
     text = await ct("🥺 Artık ekrandan çok uzaktayım..")
     await message.edit_text(text)
+
+
+@muinrobot()
+async def _(_):
+    if AFKMOD:
+        it = await it('😆 Artık afk değilim!')
+        await bot.send_message(_.chat.id,it)
+        AFKMOD = False
+    await message.continue_propagation()
