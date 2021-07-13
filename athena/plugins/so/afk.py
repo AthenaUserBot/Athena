3f72934a4144ebb3b068208fe9918eebc71cbd7f
berce <75228263+must4f@users.noreply.github.com>
2021-07-13 23:43:21 +0300

    Create afk.py

---
+++ athena/plugins/so/afk.py
@@ -0,0 +1,10 @@
+from athena import AFKMOD
+from core.muin import muinrobot
+from athena.func import it, ct
+
+@muinrobot(pattern='^.afk')
+async def afkmodon(message):
+    global AFKMOD
+    if AFMOD:
+        return await message.edit_text(await it('😳 Şuanda zaten a-afk'sın!'))
+    await message.edit_text(await ct('🥺 Artık ekrandan çok uzaktayım..'))
