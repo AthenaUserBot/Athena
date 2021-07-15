aa7c15288797f1cda8070ebd20880ce447b822cc
berce <75228263+must4f@users.noreply.github.com>
2021-07-16 00:36:14 +0300

    Update muin.py

--- core/muin.py
+++ core/muin.py
@@ -21,7 +21,7 @@ from athena import TEMP, BOTLOG, PREFIXES, bot as app
 
 def muinrobot(**args):
     pattern = args.get('pattern', None)
-    pattern = args.get('command', None)
+    command = args.get('command', None)
     outgoing = args.get('outgoing', True)
     incoming = args.get('incoming', False)
     noedit = args.get('noedit', False)
