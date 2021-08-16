158363bda8c5fc0994116ba52e4995071dc727ce
berce <75228263+must4f@users.noreply.github.com>
2021-08-16 14:13:56 +0300

    Update alive.py

--- athena/plugins/sys/alive.py
+++ athena/plugins/sys/alive.py
@@ -4,4 +4,7 @@ from athena import BOT_NAME
 
 @muinrobot(pattern='^.alive$')
 async def alive(message):
-    await message.edit_text(f"❤️ ** < bu sana 🥺 Athena Senin için Çalışıyor!**")
+    await message.edit_text(f"""
+❤️** < bu sana 🥺
+{BOT_NAME} Senin için Çalışıyor!**
+""")
