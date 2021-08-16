bc7ba879c0c8f646554611a66a04992df05e7ab0
berce <75228263+must4f@users.noreply.github.com>
2021-08-16 14:28:57 +0300

    iyileştirmeler

--- athena/func/__init__.py
+++ athena/func/__init__.py
@@ -2,19 +2,19 @@ async def extract_text(message):
     text = message.text.markdown or message.caption.markdown
     if not text:
         return ''
-    return text.split()[1] if len(text.split()) > 1 else None
+    return text.split(None,1)[1] if len(text.split()) > 1 else None
 
 async def to_be_sent(chat):
     from athena import BOTLOG
     return BOTLOG if BOTLOG else chat.id
 
 async def it(text):
-    return '<i>' + text + '</i>'
+    return '<i>' + str(text) + '</i>'
 
 async def bt(text):
-    return '<i>' + text + '</b>'
+    return '<i>' + str(text) + '</b>'
 
 async def ct(text):
-    return '<code>' + text + '</code>'
+    return '<code>' + str(text) + '</code>'
 
 
