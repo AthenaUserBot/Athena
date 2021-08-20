7c2d73e249956e81f61ca18b5031073dc44474ca
berce <75228263+must4f@users.noreply.github.com>
2021-08-20 17:28:07 +0300

    Update zup.py

--- athena/plugins/extra/zup.py
+++ athena/plugins/extra/zup.py
@@ -2,11 +2,8 @@ from athena import bot as app
 import os
 
 def tg_userbotinstaller():
-    for message in app.iter_history("me"):
-        if message.document:
-            ret_msg = message.document
-        else:
-            continue
+    for message in app.search_messages("me", filter="document"):
+        ret_msg = message.document
         file_name = ret_msg.file_name
         if (len(file_name.split('.')) > 1) \
             and plugin.file.name.split('.')[-1] == 'py':
