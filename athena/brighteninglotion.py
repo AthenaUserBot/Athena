d9040c54ca93d6be2e6777c27efdea2cd271dab9
berce <75228263+must4f@users.noreply.github.com>
2021-08-21 20:37:45 +0300

    Plugins

--- athena/brighteninglotion.py
+++ athena/brighteninglotion.py
@@ -1,8 +1,18 @@
 from . import *
 from pyrogram import idle
+from .plugins.extra.zup import tg_userbotinstaller
 
 try:
+    bot.stop()
+except:
+    print("Athena starting...")
+finally:
     bot.start()
+    tg_userbotinstaller()
+
+bot.set_parse_mode()
+
+try:
     print('Athena çalışıyor. Sorununuz varsa t.me/athenasupport a gelin.')
     idle()
 except Exception as e:
