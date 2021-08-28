6dcc81da189500ce336a5d481c821d3f5541f4ea
berce <75228263+must4f@users.noreply.github.com>
2021-08-28 12:33:47 +0300

    Create __init__.py

---
+++ athena/plugins/__init__.py
@@ -0,0 +1,26 @@
+"""''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+┏━━━━━━━━━━━━━━━━━━━━━━━━░🔥░━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+┣➤                                ✨ 𝑩𝑬𝑹𝑪𝑬𝑺𝑻𝑬 ✨
+┣➤                 🥇 Copyright:. <github.com/Athenauserbot> 🥇
+┗━━━━━━━━━━━━━━━━━━━━━━━━░🔥░━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''"""
+
+
+from athena import LOGS
+
+
+def __list_all_modules():
+    from os.path import dirname, basename, isfile
+    import glob
+
+    mod_paths = glob.glob(dirname(__file__) + "/*.py")
+    all_modules = [
+        basename(f)[:-3] for f in mod_paths
+        if isfile(f) and f.endswith(".py") and not f.endswith("__init__.py")
+    ]
+    return all_modules
+
+
+ALL_MODULES = sorted(__list_all_modules())
+LOGS.info("Modüller: " + str(ALL_MODULES))
+__all__ = ALL_MODULES + ["ALL_MODULES"]
