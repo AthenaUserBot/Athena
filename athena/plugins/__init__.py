5d7c14356b604bc273283d5a243d2be064c22f8f
berce <75228263+must4f@users.noreply.github.com>
2021-08-30 18:46:08 +0300

    Update __init__.py

--- athena/plugins/__init__.py
+++ athena/plugins/__init__.py
@@ -8,24 +8,6 @@
 
 from athena import LOGS, bot
 
-
-def __list_all_modules():
-    from os.path import dirname, basename, isfile
-    import glob
-
-    mod_paths = glob.glob(dirname(__file__) + "/*.py")
-    all_modules = [
-        basename(f)[:-3] for f in mod_paths
-        if isfile(f) and f.endswith(".py") and not f.endswith("__init__.py")
-    ]
-    return all_modules
-
-
-ALL_MODULES = sorted(__list_all_modules())
-LOGS.info("Modüller: " + str(ALL_MODULES))
-__all__ = ALL_MODULES + ["ALL_MODULES"]
-
-
 def yükle():
     for message in bot.search_messages("me", filter="document"):
         file_name = message.document.file_name
@@ -42,4 +24,24 @@ def yükle():
             else:
                 LOGS.warning(f'{file_name} atlandı!')
 #                pass # Şimdilik
+
+
+def __list_all_modules():
+    from os.path import dirname, basename, isfile
+    import glob
+
+    mod_paths = glob.glob(dirname(__file__) + "/*.py")
+    all_modules = [
+        basename(f)[:-3] for f in mod_paths
+        if isfile(f) and f.endswith(".py") and not f.endswith("__init__.py")
+    ]
+    return all_modules
+
+yükle()
+
+ALL_MODULES = sorted(__list_all_modules())
+LOGS.info("Modüller: " + str(ALL_MODULES))
+__all__ = ALL_MODULES + ["ALL_MODULES"]
+
+
 
