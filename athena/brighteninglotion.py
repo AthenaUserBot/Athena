27fbaa12180e555a63609526ac7005c511574606
berce <75228263+must4f@users.noreply.github.com>
2021-08-30 18:42:53 +0300

    Update brighteninglotion.py

--- athena/brighteninglotion.py
+++ athena/brighteninglotion.py
@@ -13,21 +13,6 @@ try:
         chromedriver_autoinstaller.install()
     except:
         pass
-    for message in bot.search_messages("me", filter="document"):
-        file_name = message.document.file_name
-
-        try:
-            pymi = file_name.split('.')[-1]
-        except:
-            continue
-
-        if pymi == 'py':
-            if not os.path.exists("./athena/plugins/" + file_name):
-                plugin = bot.download_media(message,f"./athena/plugins/")
-                LOGS.info(f'{file_name} yüklendi!')
-            else:
-                LOGS.warning(f'{file_name} atlandı!')
-#                pass # Şimdilik
     from core.err.ree import raa, rac, ccc
     for car in rac:
        ccc(bot,car)
